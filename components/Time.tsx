b3ffde0eabe966dbd60596424d5cd98968e8ee17
skidoodle <hello@albrt.hu>
2022-04-02 23:54:19 +0200

    🔥

--- components/Time.tsx
+++ components/Time.tsx
@@ -14,14 +14,7 @@ const Timer = () => {
 
     return(
         <div className={styles.time}>
-<<<<<<< HEAD
             <p><FaClock /> {date.toLocaleDateString('en-GB', { dateStyle: 'short' }) + ' • ' + date.toLocaleTimeString('en-GB', { timeStyle: 'medium', hour12: true,  timeZone: 'Europe/Budapest'}).toUpperCase()}</p>
-=======
-            <p><FontAwesomeIcon icon={['fas', 'clock']} /> {date.toLocaleString('en-GB', {
-                timeZone: 'Europe/Budapest',
-                hour12: true
-            }).toUpperCase()}</p>
->>>>>>> ce3c841bfc958c99053e0a03c56b5cbc56dff70f
         </div>
     )
 }
