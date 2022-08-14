334506ba2d6f8d29988a7fdfa269e62571b3afd1
skidoodle <53189968+skidoodle@users.noreply.github.com>
2022-08-14 22:54:40 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,7 @@ export const Footer = () => {
     return(
         <div className='text-center text-lg'>
             <footer className='fixed bottom-2 left-0 right-0 text-[#727277] font-semibold'>
-                <a href='https://arch.gay' target='_blank'>arch</span>
-                </a>
+                <a href='https://arch.gay' target='_blank'>arch</a>
             </footer>
         </div>
     )
