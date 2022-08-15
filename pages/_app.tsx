a93d09feb8411fdbcd9e97676d3d9ec15f482277
skidoodle <skidoodle>
2022-08-15 13:35:47 +0200

    html language

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,9 @@
 import 'styles/globals.scss'
-import Head from 'next/head'
 
 import { AppProps } from 'next/app'
 
 export default function({ Component, pageProps }: AppProps) {
-    return(
-        <>
-            <Head>
-                    <link rel='preconnect' href='https://vitals.vercel-insights.com' />
-                    <title>albert</title>
-                    <meta name='title' content='albert' />
-                    <meta name='og:title' content='albert' />
-                    <meta name='description' content='system administrator' />
-                    <meta name='og:description' content='system administrator' />
-                    <meta name='theme-color' content='#000000' />
-                    <meta property='og:image' content='/favicon.ico' />
-            </Head>
-            <Component {...pageProps} />
-        </>
+    return (
+        <Component {...pageProps} />
     )
 }
