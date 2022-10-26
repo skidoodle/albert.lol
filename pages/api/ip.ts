3300c1f45a8ab04458e7bbba067a05b043a6ba5d
skidoodle <53189968+skidoodle@users.noreply.github.com>
2022-10-26 22:34:40 +0200

    api test 5 debug

--- pages/api/ip.ts
+++ pages/api/ip.ts
@@ -5,5 +5,6 @@ export default async function ip(req: NextApiRequest, res: NextApiResponse){
     let xff = req.headers['x-forwarded-for'];
     let xri = req.headers['x-real-ip'];
     let cra = req.connection.remoteAddress;
-    res.status(200).json({ cf: cf, xff: xff, xri: xri, cra: cra });
+    let sra = req.socket.remoteAddress;
+    res.status(200).json({ cf: cf, xff: xff, xri: xri, cra: cra, sra: sra });
 }
