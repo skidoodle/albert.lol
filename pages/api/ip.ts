bcc7e84db7257ad87cd2656d31c30f9240528e42
skidoodle <53189968+skidoodle@users.noreply.github.com>
2022-10-26 21:11:55 +0200

    ip api test 2

--- pages/api/ip.ts
+++ pages/api/ip.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function ip(req: NextApiRequest, res: NextApiResponse){
-    const ip = (req.headers['x-forwarded-for' || 'x-vercel-forwarded-for']);
+    let ip = req.headers['cf-connecting-ip']
     res.json({ ip: ip });
 }
