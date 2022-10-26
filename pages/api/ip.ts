0170d70bd79eb710d683ab78a377c3a6a5187bcd
skidoodle <53189968+skidoodle@users.noreply.github.com>
2022-10-26 22:43:09 +0200

    ip api test 6

--- pages/api/ip.ts
+++ pages/api/ip.ts
@@ -2,9 +2,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function ip(req: NextApiRequest, res: NextApiResponse){
     let cf = req.headers['cf-connecting-ip'];
+    let cf2 = req.headers['Cf-Pseudo-IPv4'];
     let xff = req.headers['x-forwarded-for'];
     let xri = req.headers['x-real-ip'];
-    let cra = req.connection.remoteAddress;
-    let sra = req.socket.remoteAddress;
-    res.status(200).json({ cf: cf, xff: xff, xri: xri, cra: cra, sra: sra });
+    res.status(200).json({ cf: cf, cf2: cf2, xff: xff, xri: xri });
 }
