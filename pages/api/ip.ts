d5d68a2cb30b8e0c537edc6417c79f5d98e606d7
skidoodle <53189968+skidoodle@users.noreply.github.com>
2022-10-26 22:53:03 +0200

    fix ip api

--- pages/api/ip.ts
+++ pages/api/ip.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function ip(req: NextApiRequest, res: NextApiResponse){
-    let cf = req.headers['cf-connecting-ip'];
-    let cf2 = req.headers['Cf-Pseudo-IPv4'];
-    let xff = req.headers['x-forwarded-for'];
-    let xri = req.headers['x-real-ip'];
-    res.status(200).json({ cf: cf, cf2: cf2, xff: xff, xri: xri });
+    let ip = req.headers['cf-connecting-ip'];
+    res.status(200).json({ ip: ip });
 }
