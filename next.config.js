496f6bdb33d793c4f63337a3759eb6078fd1c755
skidoodle <53189968+skidoodle@users.noreply.github.com>
2023-02-16 17:45:30 +0100

    -interest-cohort

--- next.config.js
+++ next.config.js
@@ -29,7 +29,7 @@ const securityHeaders = [
   },
   {
     key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+    value: 'camera=(), microphone=(), geolocation=()',
   },
 ];
 
