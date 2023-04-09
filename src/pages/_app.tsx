59d88339bd0427257ef35ff5c544dc84eb8ef249
skidoodle <53189968+skidoodle@users.noreply.github.com>
2023-04-09 11:40:21 +0200

    .use next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,13 @@ import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { ThemeProvider } from 'next-themes';
 import Head from 'next/head';
+import { Inter } from 'next/font/google';
 import '@/styles/globals.scss';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,9 +16,11 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>albert</title>
       </Head>
-      <ThemeProvider>
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <main className={`${inter.variable} font-sans`}>
+        <ThemeProvider>
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </main>
       <Analytics />
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
+    },
+  },
   plugins: [],
 };
