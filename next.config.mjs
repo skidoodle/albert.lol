a29b8a0bd919e055db2dd1affc9ab55f10d5bdd3
skidoodle <contact@albert.lol>
2024-01-09 19:42:44 +0100

    million

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import million from 'million/compiler'
+
 const securityHeaders = [
   {
     key: 'X-DNS-Prefetch-Control',
@@ -33,7 +35,8 @@ const securityHeaders = [
   },
 ]
 
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   async headers() {
     return [
       {
@@ -53,3 +56,9 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
 }
+
+const millionConfig = {
+  auto: true,
+}
+
+export default million.next(nextConfig, millionConfig)
