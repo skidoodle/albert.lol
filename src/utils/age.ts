5c3c7c884dfa3ff13ad6a827bae3e2c1fb8f28e5
István <156918376+xXxMLGGamerXxx@users.noreply.github.com>
2024-01-24 15:47:04 +0100

    fix: opsec blunder
    Signed-off-by: István  <156918376+xXxMLGGamerXxx@users.noreply.github.com>

--- src/utils/age.ts
+++ src/utils/age.ts
@@ -1,6 +1,14 @@
+function e(input) {
+  return btoa(input); 
+}
+
+function d(input) {
+  return atob(input); // 
+}
+
 export default function age() {
-  return Math.floor(
-    (new Date().getTime() - new Date('2004-07-22').getTime()) /
-      (1000 * 60 * 60 * 24 * 365.25)
-  )
+  const ed = 'Smw3Z2FhNi0yMjBi'; 
+  const de = d(ed);
+  const age = Math.floor((new Date().getTime() - new Date(de).getTime()) / (1000 * 60 * 60 * 24 * 365.25));
+  return age;
 }
