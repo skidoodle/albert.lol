380ec47afd90bede42e057439ac4b4659d354cd7
skidoodle <contact@albert.lol>
2024-02-09 21:59:30 +0100

    fix margin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,14 +10,16 @@ export default function Home() {
     <>
       <ThemeSwitcher />
       <FadeIn>
-        <div className='mx-auto mb-16 mt-32 flex max-w-3xl flex-col'>
-          <h1 className='text-[5rem] leading-none font-bold'>albert</h1>
-          <p className='mt-2 text-[1.6rem] leading-none font-semibold text-slate-600'>
-            {age()}yo devops engineer
-          </p>
-          <SocialLayout />
-          <NowPlayingCard />
-          <Toaster position='top-left' />
+        <div className='ml-[10%] mr-[10%]'>
+          <div className='mx-auto mb-16 mt-32 flex max-w-3xl flex-col'>
+            <h1 className='text-[5rem] leading-none font-bold'>albert</h1>
+            <p className='mt-2 text-[1.6rem] leading-none font-semibold text-slate-600'>
+              {age()}yo devops engineer
+            </p>
+            <SocialLayout />
+            <NowPlayingCard />
+            <Toaster position='top-left' />
+          </div>
         </div>
       </FadeIn>
     </>
