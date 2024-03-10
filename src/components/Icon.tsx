a70766861590d54e3a2f5dfac6eea3cffdea476e
skidoodle <contact@albert.lol>
2024-03-10 19:50:44 +0100

    Refactor Icon component to improve code readability and maintainability

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -5,40 +5,31 @@ import Link from 'next/link'
 import type { IconType } from '@/utils/types'
 
 const notify = () => {
-  toast.remove(),
-    toast.success('Copied to clipboard', {
-      style: {
-        background: '#0f1012',
-        color: '#fff',
-        fontSize: '1em',
-      },
-    })
+  toast.remove()
+  toast.success('Copied to clipboard', {
+    style: {
+      background: '#0f1012',
+      color: '#fff',
+      fontSize: '1em',
+    },
+  })
 }
 
 export const Icon = ({ children, reference, copyValue }: IconType) => {
-  if (copyValue) {
-    return (
-      <Link
-        href={''}
-        className={`cursor-pointer`}
-        aria-label={
-          Socials.find((social) => social.ref === reference)?.ariaLabel
-        }
-        onClick={() => {
-          notify(), copy(reference)
-        }}
-      >
-        {children}
-      </Link>
-    )
+  const social = Socials.find((social) => social.ref === reference)
+
+  const handleCopy = () => {
+    notify()
+    copy(reference)
   }
 
   return (
     <Link
-      href={reference}
-      target='_blank'
-      className={'cursor-pointer'}
-      aria-label={Socials.find((social) => social.ref === reference)?.ariaLabel}
+      href={copyValue ? '' : reference}
+      target={copyValue ? undefined : '_blank'}
+      className='cursor-pointer'
+      aria-label={social?.ariaLabel}
+      onClick={copyValue ? handleCopy : undefined}
     >
       {children}
     </Link>
