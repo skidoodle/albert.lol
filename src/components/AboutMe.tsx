0f743eb62cb7283f438b9d0584b2f15ee5b10ad7
skidoodle <contact@albert.lol>
2024-09-15 03:33:53 +0200

    szeretjuk a szineket

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,19 +1,18 @@
-import age from '@/utils/age'
+import age from '@/utils/age';
 
 export const AboutMe = () => {
   return (
-    <section className='p-6 max-w-lg rounded-lg shadow-lg'>
+    <section className='p-6 max-w-lg rounded-lg shadow-lg mt-2'>
       <h2 className='text-3xl font-bold mb-4'>About Me</h2>
       <p className='text-base leading-relaxed'>
-        I’m a {age()}-year-old Computer Science student based in Budapest,
-        Hungary with a deep passion for systems engineering and technology. I
-        enjoy working on my homelab and self-hosting projects, and I’m also
-        enthusiastic about coding in TypeScript and Go.
-      </p>
-      <p className='text-base leading-relaxed mt-4'>
-        If you share similar interests or simply want to chat, feel free to
-        reach out!
+        I’m <span className='font-semibold text-blue-400'>{age()} years old</span>,{' '}
+        studying <span className='font-medium text-orange-400'>Computer Science Operational Engineering</span>{' '}
+        at the <span className='font-medium text-green-400'>University of Óbuda</span> in Hungary.{' '}
+        I’m passionate about <span className='text-red-400'>systems engineering</span>,{' '}
+        working on my <span className='italic text-teal-300'>homelab</span>,{' '}
+        and coding in <span className='font-medium text-purple-400'>TypeScript</span>{' '}
+        and <span className='font-medium text-purple-400'>Go</span>.
       </p>
     </section>
-  )
-}
+  );
+};
